53977be229211eeee82033370f2c9fbb05a7b78e
Klaudia Matis <klaudia_matis@epam.com>
2018-07-01 21:57:09 +0200

    create a state with examples

--- src/App.js
+++ src/App.js
@@ -2,9 +2,41 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Header from './header/Header';
 import Input from './inputs/Input';
+import uuid from 'uuid';
 
-class App extends Component {
+const createEntry = (isIncome, amount, date, label) => ({
+    uuid: uuid(), isIncome, amount, date, label
+});
+
+let today = new Date();
+let initialEntries = [
+    createEntry(true, 234, (today.setDate( today.getDate -10 )).toString(), "Income 1"),
+    createEntry(true, 24, (today.setDate( today.getDate -6 )).toString(), "Income 2"),
+    createEntry(true, 72, (today.setDate( today.getDate -2 )).toString(), "Income 3"),
+    createEntry(true, 12, (today.setDate( today.getDate -12 )).toString(), "Spending 1"),
+    createEntry(true, 2, (today.setDate( today.getDate -5 )).toString(), "Spending 2"),
+    createEntry(true, 31, (today.setDate( today.getDate -4 )).toString(), "Spending 3"),
+    createEntry(true, 122, (today.setDate( today.getDate -2 )).toString(), "Spending 4"),
+]
+
+export default class App extends Component {
+    state = {
+        entries: initialEntries,
+        grandTotal : 0
+    }
+
+    componentDidMount(){
+        this.setState({grandTotal: this.getGrandTotal()});
+    }
     
+    getGrandTotal = () => {
+        let total = 0;
+        this.state.entries.forEach(element => {
+            let multiplier = element.isIncome ? 1 : -1;
+            total += multiplier * element.amount;
+        });
+        return total;
+    }
 
     render () {
         return (
@@ -14,5 +46,3 @@ class App extends Component {
         </div>);
     };
 }
-
-export default App;
--- src/inputs/Input.js
+++ src/inputs/Input.js
@@ -25,7 +25,6 @@ class Input extends React.Component {
         });
     }
 
-
     render(){
         return(
             <div>
