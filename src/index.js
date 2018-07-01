49269d4c1ad29e147f9d21c7589d01629626cc7b
Klaudia Matis <klaudia_matis@epam.com>
2018-07-01 22:07:39 +0200

    some reorganization, no code change

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './component/App';
 
 const node = document.createElement('div');
 document.body.appendChild(node);
