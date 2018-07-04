528e4eb9995fc8672b79807c1ef1f8841e4c9e8d
Klaudia Matis <klaudia_matis@epam.com>
2018-07-05 00:30:13 +0200

    type validation with PropTypes

--- src/component/output/Entry.js
+++ src/component/output/Entry.js
@@ -3,10 +3,18 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 
+const defaultEntry =  {
+    isIncome: true,
+    amount: 0,
+    key: new Date(),
+    info: "No text, this is a default valued entry",
+    date: moment()
+};
+
 export default class Entry extends Component{
     render(){
         {console.log(this.props)}
-        const entry = this.props.entry;
+        const entry = {...defaultEntry, ...this.props.entry};
         return(
             <DifferedEntries isIncome={entry.isIncome}>
                 <label>{entry.amount}</label>
@@ -23,19 +31,21 @@ const DifferedEntries = styled.section`
     color: ${props => props.isIncome ? 'green' : 'red'}
 `;
 
-Entry.defaultProps = {
-    entry:{
-        isIncome: true,
-        amount: 23432456356,
-        key: new Date(),
-        info: "No text",
-        date: moment()
-    }
-}
+// not working on object for me, only on primitive types, one level
+// Entry.defaultProps = {
+//     entry:  {
+//         isIncome: true,
+//         amount: 23432456356,
+//         key: new Date(),
+//         info: "No text",
+//         date: moment()
+//     }
+// };
 
 Entry.propTypes = {
     entry: PropTypes.shape({
         isIncome: PropTypes.bool,
+        amount: PropTypes.number,
         key: PropTypes.string,
         info: PropTypes.string,
         date: PropTypes.object
