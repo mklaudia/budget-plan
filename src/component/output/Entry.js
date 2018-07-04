372d0da31f0ebcdf03dcced384c2e3759d107098
Klaudia Matis <klaudia_matis@epam.com>
2018-07-05 00:03:49 +0200

    proptype checks

--- src/component/output/Entry.js
+++ src/component/output/Entry.js
@@ -1,8 +1,11 @@
 import React, {Component} from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import moment from 'moment';
 
 export default class Entry extends Component{
     render(){
+        {console.log(this.props)}
         const entry = this.props.entry;
         return(
             <DifferedEntries isIncome={entry.isIncome}>
@@ -19,3 +22,22 @@ export default class Entry extends Component{
 const DifferedEntries = styled.section`
     color: ${props => props.isIncome ? 'green' : 'red'}
 `;
+
+Entry.defaultProps = {
+    entry:{
+        isIncome: true,
+        amount: 23432456356,
+        key: new Date(),
+        info: "No text",
+        date: moment()
+    }
+}
+
+Entry.propTypes = {
+    entry: PropTypes.shape({
+        isIncome: PropTypes.bool,
+        key: PropTypes.string,
+        info: PropTypes.string,
+        date: PropTypes.object
+    })
+}
