ed4b8d50e0aad66d15ee8b0a4cf1c23c20515fae
Klaudia Matis <klaudia_matis@epam.com>
2018-07-05 18:09:47 +0200

    clean up

--- src/component/filter/FilterButton.js
+++ src/component/filter/FilterButton.js
@@ -5,6 +5,6 @@ export default class FilterButton extends Component{
 
     render(){
         let p = this.props;
-        return <a className={p.className}  href={p.href} onClick={this.onClick}>{p.value}</a>;
+        return <a className={p.className} href={p.href}>{p.value}</a>;
     }
 }
