5305e06c6dd6d79203b50c080ece675a22c578c7
Klaudia Matis <klaudia_matis@epam.com>
2018-07-05 10:56:28 +0200

    introduce react router

--- src/component/output/EntryList.js
+++ src/component/output/EntryList.js
@@ -1,12 +1,33 @@
 import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import Entry from './Entry';
 
 export default class EntryList extends Component{
     render(){
         const  {entries}  = this.props ;
+        
+        const getAllEntries = () => {
+            return entries;
+        }
+
         return(
             <div>
-                {entries.map(entry => <Entry key={entry.key} entry={entry}/>)}
+                <Router>
+                    <div>
+                    <Route exact path="/" render={props => {
+                            console.log(props)
+                            return(
+                            <div>Hello You
+
+                                 {entries.map(entry => <Entry key={entry.key} entry={entry}/>)}
+                            </div>);
+                            // {entries.map(entry => <Entry key={entry.key} entry={entry}/>)}
+                        }} 
+                    />
+
+                    </div>
+				</Router>
+                {/* {entries.map(entry => <Entry key={entry.key} entry={entry}/>)} */}
             </div>
         );
     }
